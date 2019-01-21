d2d4c966916c0b7015a113b2d373660ca48cd508
Sparky <sparky@possumlodge.me>
2019-01-21 15:50:22 -0500

    Removed unused import

--- jellyfinclient/src/screens/EntryScreen.js
+++ jellyfinclient/src/screens/EntryScreen.js
@@ -1,7 +1,6 @@
 import React, {Component} from 'react';
 import {connect} from "react-redux";
 
-import  SampleAction from './../actions/SampleAction';
 import TopLevelComponent from '../components/TopLevelComponent'
 
 
